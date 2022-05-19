aaf6b6eb37a95fcac232cb9b57af13e0c66773dd
Ahmed Fawzy <76911354+0xluki@users.noreply.github.com>
2022-05-19 18:51:24 +0200

    Update mail-client.js

--- util/mail-client.js
+++ util/mail-client.js
@@ -1,9 +1,11 @@
 const nodemailer = require('nodemailer');
 const transport = require('nodemailer-sendgrid-transport');
 
+const API_KEY = 'YOUR_SEND_GRID_API_KEY'
+
 const client = nodemailer.createTransport(transport({
     auth:{
-        api_key:'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'
+        api_key:API_KEY
     }
 }))
 
